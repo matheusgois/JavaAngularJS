a7e4b5437330c765beccb2747b36e81fee169be4
matheusgois <matheusdc77@gmail.com>
2016-11-09 11:15:00 -0200

    sem erro

--- src/main/java/com/stefanini/model/Telefones.java
+++ src/main/java/com/stefanini/model/Telefones.java
@@ -35,9 +35,9 @@ public class Telefones implements Serializable {
 
 	@EmbeddedId
 
-	@AttributeOverrides({ @AttributeOverride(name = "numero", column = @Column(name = "numero", nullable = false)),
-			@AttributeOverride(name = "ddd", column = @Column(name = "ddd", nullable = false)),
-			@AttributeOverride(name = "cpfProprietario", column = @Column(name = "cpfProprietario", nullable = false)) })
+	/*@AttributeOverrides({ @AttributeOverride(name = "numero", column = @Column(name = "numero", nullable = true)),
+			@AttributeOverride(name = "ddd", column = @Column(name = "ddd", nullable = true)),
+			@AttributeOverride(name = "cpfProprietario", column = @Column(name = "cpfProprietario", nullable = true)) })*/
 	public TelefonesId getId() {
 		return this.id;
 	}
